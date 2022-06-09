72fc7d3e1806d0906a9d87335000bab3fe05f278
FUSIOZ031 <62032230+FUSIOZ031@users.noreply.github.com>
2022-06-09 10:58:20 +0700

    Update git_repos_jobportal.sql

--- git_repos_jobportal.sql
+++ git_repos_jobportal.sql
@@ -1,12 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 4.6.5.2
--- https://www.phpmyadmin.net/
---
--- Host: localhost:8889
--- Generation Time: Aug 10, 2019 at 04:47 PM
--- Server version: 5.6.35
--- PHP Version: 7.0.15
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
